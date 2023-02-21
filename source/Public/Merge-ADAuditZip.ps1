7dc7d8f876db6d2aa92966444f96b52025fbf075
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2023-02-20 18:04:45 -0600

    docs: Update help comment block

--- source/Public/Merge-ADAuditZip.ps1
+++ source/Public/Merge-ADAuditZip.ps1
@@ -1,8 +1,44 @@
 function Merge-ADAuditZip {
+<#
+    .SYNOPSIS
+    Combines multiple audit report files into a single compressed ZIP file.
+
+    .DESCRIPTION
+    The Merge-ADAuditZip function combines multiple audit report files into a single compressed ZIP file. The function takes an array of file paths, a maximum file size for the output ZIP file, an output folder for the merged file, and an optional switch to open the directory of the merged file after creation.
+
+    .PARAMETER FilePaths
+    Specifies an array of file paths to be merged into a single compressed ZIP file.
+
+    .PARAMETER MaxFileSize
+    Specifies the maximum file size (in bytes) for the output ZIP file. The default value is 24 MB.
+
+    .PARAMETER OutputFolder
+    Specifies the output folder for the merged compressed ZIP file. The default folder is C:\temp.
+
+    .PARAMETER OpenDirectory
+    Specifies an optional switch to open the directory of the merged compressed ZIP file after creation.
+
+    .EXAMPLE
+    $workstations = Get-ADHostAudit -HostType WindowsWorkstations -Report
+    $servers = Get-ADHostAudit -HostType WindowsServers -Report
+    $nonWindows = Get-ADHostAudit -HostType "Non-Windows" -Report
+    Merge-ADAuditZip -FilePaths $workstations, $servers, $nonWindows
+
+    This example combines three audit reports for Windows workstations, Windows servers, and non-Windows hosts into a single compressed ZIP file.
+
+    .EXAMPLE
+    Merge-ADAuditZip -FilePaths C:\AuditReports\Report1.csv,C:\AuditReports\Report2.csv -MaxFileSize 50MB -OutputFolder C:\MergedReports -OpenDirectory
+
+    This example merges two audit reports into a single compressed ZIP file with a maximum file size of 50 MB, an output folder of C:\MergedReports, and opens the directory of the merged compressed ZIP file after creation.
+
+    .NOTES
+    This may or may not work with various types of input.
+#>
+
     param(
-        [string[]]$FilePaths,  # Array of file paths to be merged into a single zip file
-        [int]$MaxFileSize = 24MB,  # Maximum size (in bytes) of the output zip file
-        [string]$OutputFolder = "C:\temp",  # Output path of the merged zip file
+        [string[]]$FilePaths, # Array of file paths to be merged into a single zip file
+        [int]$MaxFileSize = 24MB, # Maximum size (in bytes) of the output zip file
+        [string]$OutputFolder = "C:\temp", # Output path of the merged zip file
         [switch]$OpenDirectory  # Optional switch to open the directory of the merged zip file after creation
     )
 
@@ -34,7 +70,8 @@ function Merge-ADAuditZip {
     $currentSize = 0
     $filesToAdd = @()
     foreach ($filePath in $sortedFiles) {
-        if (($currentSize + $fileSizes[$filePath]) -gt $MaxFileSize) {  # If adding the next file would exceed the maximum size
+        if (($currentSize + $fileSizes[$filePath]) -gt $MaxFileSize) {
+            # If adding the next file would exceed the maximum size
             # Create a zip file with the current batch of files
             Compress-Archive -Path $filesToAdd -DestinationPath $OutputPath -Update
             $filesToAdd = @()  # Clear the list of files to add
@@ -52,7 +89,8 @@ function Merge-ADAuditZip {
         Remove-Item -Path $filePath -Force
     }
 
-    if ($OpenDirectory) {  # If the OpenDirectory switch is used
+    if ($OpenDirectory) {
+        # If the OpenDirectory switch is used
         Invoke-Item (Split-Path $OutputPath)  # Open the directory of the merged zip file
     }
     else {
