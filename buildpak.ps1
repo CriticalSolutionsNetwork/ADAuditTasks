294d065007ea9837b0210c9777d600918efce4ee
DrIOS <58635327+DrIOSX@users.noreply.github.com>
2023-02-21 13:53:38 -0600

    docs: Updated build scratch

--- buildpak.ps1
+++ buildpak.ps1
@@ -8,10 +8,28 @@ Remove-Item .\output\CHANGELOG.md
 
 #.\build.ps1 -ResolveDependency -tasks noop
 #.\build.ps1 -tasks build -CodeCoverageThreshold 0
-
+<#
+1. Merge the pull request that contains the code documentation and comments.
+2. Switch to the main branch in your local repository using git checkout main.
+3. Pull the latest changes from the remote repository using git pull origin main.
+4. Verify that the module works as expected by importing it into a PowerShell session and running the cmdlets.
+5. Tag the main branch with the version number 0.1.7 using git tag -a v0.1.7 -m "Release version 0.1.7".
+6. Push the tag to the remote repository using git push origin v0.1.7.
+7. Run the build using the pack task to create the NuGet package: .\build.ps1 -tasks pack -CodeCoverageThreshold 0.
+8. Upload the NuGet package to the PowerShell Gallery using the publish task: .\build.ps1 -tasks publish -CodeCoverageThreshold 0.
+#>
 git tag -a v0.1.1 -m "Fix Update"
 git tag -d v1.0.0
-
+$workstations = Get-ADHostAudit -HostType WindowsWorkstations -Report -Verbose
+$servers = Get-ADHostAudit -HostType WindowsServers -Report -Verbose
+$nonWindows = Get-ADHostAudit -HostType "Non-Windows" -Report -Verbose
+$activeUsers = Get-ADActiveUserAudit -Report -Verbose
+$privilegedUsers = Get-ADUserPrivilegeAudit -Report -Verbose
+$wildcardUsers = Get-ADUserWildCardAudit -WildCardIdentifier "svc" -Report -Verbose
+Merge-ADAuditZip -FilePaths $workstations, $servers, $nonWindows, $activeUsers, $privilegedUsers, $wildcardUsers
+Get-HostTag -PhysicalOrVirtual Physical -Prefix "CRP" -SystemOS 'Windows Server' -ErrorAction
+Get-ADUserLogonAudit -SamAccountName "<USERNAME>" -Verbose
+Get-NetworkAudit -LocalSubnets -Report -Verbose
 # Update Changelog
 # Update Manifest from Previous Module
 # git tag -a v1.0.0 -m "v1.0.0 Release"
@@ -21,3 +39,8 @@ git tag -d v1.0.0
 # Build / Publish
 
 .\build.ps1 -tasks build,pack,publish -CodeCoverageThreshold 0
+
+$workstations = Get-ADHostAudit -HostType WindowsWorkstations -Report
+$servers = Get-ADHostAudit -HostType WindowsServers -Report
+$nonWindows = Get-ADHostAudit -HostType "Non-Windows" -Report
+Merge-ADAuditZip -FilePaths $workstations, $servers, $nonWindows
