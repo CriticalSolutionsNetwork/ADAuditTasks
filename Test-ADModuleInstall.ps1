4f2ea14a91e838edc25e55cbb4a5829bf1638df5
Doug Rios <58635327+DrIOSX@users.noreply.github.com>
2023-03-12 18:25:38 -0500

    Delete Test-ADModuleInstall.ps1

--- Test-ADModuleInstall.ps1
+++
@@ -1,7 +0,0 @@
-function Test-ADModuleInstall {
-    # Create logging object
-    $Script:LogString = @()
-    # Begin Logging
-    $Script:LogString += Write-AuditLog -Message "Begin Log"
-    Install-ADModule
-}
